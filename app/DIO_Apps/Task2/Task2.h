c10c895720aef9f2b0d42362774791510a9c1c73
OmarAlaa8184 <omaralaa8184@gmail.com>
2025-01-09 15:08:57 +0200

    Task2.h file/v1

---
+++ app/DIO_Apps/Task2/Task2.h
@@ -0,0 +1,18 @@
+/*
+ * Task2.h
+ *
+ * Created: 12/19/2024 9:53:16 PM
+ *  Author: Omar Alaa
+ */ 
+
+
+#ifndef TASK2_H_
+#define TASK2_H_
+
+
+void BinaryCounter (void);
+
+
+
+
+#endif /* TASK2_H_ */
