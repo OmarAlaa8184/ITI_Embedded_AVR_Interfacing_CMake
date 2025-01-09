8bbcd19ee926389346467e78c06692d6e52c4617
OmarAlaa8184 <omaralaa8184@gmail.com>
2025-01-09 14:27:50 +0200

    PORT_CFG.h file/v1

---
+++ components/MCAL/MPORT/inc/PORT_CFG.h
@@ -0,0 +1,26 @@
+/*
+ * PORT_CFG.h
+ *
+ * Created: 1/1/2024 
+ * Author: Omar Alaa
+ */ 
+
+
+#ifndef PORT_CFG_H_
+#define PORT_CFG_H_
+
+
+/* object like macro to define num of ports */
+
+#define	MPORT_NUM_OF_PORTS  4
+
+/* object like macro to define num of pins */
+
+#define MPORT_NUM_OF_PINS   8
+
+
+
+
+
+
+#endif /* PORT_CFG_H_ */
