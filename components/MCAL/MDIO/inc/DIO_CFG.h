6b78bceab3a050aea83e9d502014a8e62b7b18b9
OmarAlaa8184 <omaralaa8184@gmail.com>
2025-01-09 13:29:41 +0200

    DIO_CFG.h file/v1

---
+++ components/MCAL/MDIO/inc/DIO_CFG.h
@@ -0,0 +1,25 @@
+/*
+ * DIO_CFG.h
+ *
+ * Created: 12/25/2024 2:18:08 PM
+ *  Author: Omar Alaa
+ */ 
+
+
+#ifndef DIO_CFG_H_
+#define DIO_CFG_H_
+
+/* object like macro to define num of ports */
+
+#define	MDIO_NUM_OF_PORTS  4
+
+/* object like macro to define num of pins */
+
+#define MDIO_NUM_OF_PINS   8
+
+
+
+
+
+
+#endif /* DIO_CFG_H_ */
